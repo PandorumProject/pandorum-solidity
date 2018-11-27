b346c393c9ae5b6f627d05c626e06868b153abfe
unknown <dev.jorgearturonh@gmail.com>
2018-11-27 16:12:50 -0300

    readme

---
+++ freelance-scrow.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.18;
+
+
+contract   FreelanceScrow{
+ 
+
+   
+    constructor ( ) public payable {
+        
+        //Under construction
+        
+    }
+    
+       
+    
+}
