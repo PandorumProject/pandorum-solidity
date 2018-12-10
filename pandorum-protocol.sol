28d40dd83de0dec422f0e8e343344930fa312bdc
unknown <dev.jorgearturonh@gmail.com>
2018-12-10 14:35:49 -0300

    fix

--- pandorum-protocol.sol
+++ pandorum-protocol.sol
@@ -1,12 +1,56 @@
 pragma solidity ^0.4.18;
 
+contract UserRegister{
+ 
+    struct user{
+        
+        address userAddress;
+        uint joinBlock;
+        uint userState; //1 = Registered // 2 = Blocked  // 3= Banned
+        string userHash; //some key information of the KYC will be hashed and saved in the blockchain , thats name + origin location
+   
+    }
+        //User Registry Mappings
+        mapping(uint=>user) userList;
+        uint usercount = 0;
+        address public creator;
+    // ------------------------------------------------------------------------
+    // Constructor
+          constructor() public {
+        creator = msg.sender;
+    }
 
-contract Map{
+    modifier onlyCreator {
+        require(msg.sender == creator);
+        _;
+    }
+    
+
+      
+    //Registers users in the Creative Network
+    //Assigns an address to the userID of the user mapping
+    //_userHash contains username encrypted;
+    
+    function registerUser ( address _userAddy, string _userHash) 
+    public
+    onlyCreator{
+        
+        usercount++;
+        userList[usercount].userAddress = _userAddy;
+        userList[usercount].userState = 1;
+        userList[usercount].userHash= _userHash;
+    }
+    
+}
+
+
+contract PandorumProtocol{
  
 //Idea data structure
     struct ideax{
         address creator;
         string idea;
+        string problem;
         string[] pillars;
         uint position;
         uint voteCount;
@@ -60,6 +104,11 @@ contract Map{
         
     }
     
+    ///CREATE A MATRIX TO COUNT ASSIGN TO AN IDEA THE WHOLE SET OF PILLARS BY ID 
+    ///DO THE SAME, SO EACH PILLAR, OBJETIVE AND TASK ARE LINKED TO EACH FATHER AND CHILD
+    ///USE THAT INFORMATION TO REFER TO A "NODE" STATE WHEN ASKED IN THE FUNCTIONS
+    ///SUCH AS A REWARD BY TASK, OR REWARD BY VOTING A PILLAR OR TASK OR OBJETIVE
+   
         //map of idea data structures ordered  by id;
         mapping(uint => ideax) ideaxs;
 
@@ -80,43 +129,93 @@ contract Map{
         mapping(uint=>mapping(uint=>uint)) pillarGraph;
         mapping(uint=>mapping(uint=>mapping(uint=>uint))) taskGraph;
 
-        uint userDefinitorID;    
+        uint userCount; 
+        uint ideaCount;
         uint  pillarCount;
         uint  taskCount;
         uint totalStake;
         uint proposalCount;
         
+        bool brainstormEvent;
+        
         string[] pillarList;
+        
+        address eventMaster;
         
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
 
-    constructor(string mainIdea) public {
+    constructor() public {
         
-        ideaxs[0].idea = mainIdea;
-        ideaxs[0].creator = msg.sender;
-        ideaxs[0].position = 0;
-        ideaxs[0].voteCount = 0;
+       // ideaxs[0].idea = mainIdea;
+       // ideaxs[0].creator = msg.sender;
+       // ideaxs[0].position = 0;
+       // ideaxs[0].voteCount = 0;
+          
+        eventMaster = msg.sender;
+        
+        userCount = 0;  
         pillarCount = 0;
         taskCount = 0;
         proposalCount = 0;
         totalStake = 1000;
         
+        brainstormEvent = false;
+        
         pillarList[0]="Default";
         pillarList[1]="Programming";
         pillarList[2]="Design";
-        pillarList[3]="Misc";
+        pillarList[3]="Meta-Project";
         
     }
-    //Only should be excecutable 
-    function voteIdea() public{
-        ideaxs[0].voteCount++;
-        ideaxs[0].voterList[0] = msg.sender;
+    
+    //MODIFIERS
+    
+        modifier onlyEventMaster{
+        _;   
+    }
+        modifier onlyRegistered{
+        _;   
+    }
+        modifier onlyBrainstormEvent{
+            require(brainstormEvent = true);
+        _;   
+    }
+        modifier onlyAviableMerit{
+        _;   
     }
     
-    function selectPillarDefinitor (uint userID ) public{
-        userDefinitorID = userID;
+    // EVENT MASTER FUNCTIONS
+    function startEvent() public onlyEventMaster{
+        brainstormEvent = true;
+    }
+    
+    function callCycle(uint _type) public onlyEventMaster{
+        
+        if(_type== 1){
+           //Calls Pandorum Brainstorm callCycle
+           //Checks if there is an idea with 51% or more of total users voted when cycle called.
+           //In case of true, smart-contract just assigns the held PDT to the winner idea and rewards are assigned to others.
+        }
+        
+        if(_type==2){
+            //Calls a standard Pandorum Time-Tick for 
+        }
+    }
+    
+    
+    // REGISTERED USERS FUNCTION
+    
+    function addIdea( string _idea, string _problem ) public onlyRegistered onlyBrainstormEvent {
+        ideaxs[ideaCount].idea = _idea;
+        ideaxs[ideaCount].problem = _problem;
+    }
+    
+    //Only should be excecutable 
+    function voteIdea() public onlyRegistered onlyBrainstormEvent {
+        ideaxs[0].voteCount++;
+        ideaxs[0].voterList[0] = msg.sender;
     }
     
     //This FX should only be execcutable if caller userDefinitorID
